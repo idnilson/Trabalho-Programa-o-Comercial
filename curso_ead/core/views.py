835e74b3a1acd387f35b25e1ae7caaaebb495d75
Idnilson <idnilsonn5@gmail.com>
2016-09-07 17:58:13 -0300

    Renderização do template simples : home.html na função home definida na view.py

--- curso_ead/core/views.py
+++ curso_ead/core/views.py
@@ -4,4 +4,5 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home (request):
-	return HttpResponse('Projeto Inicial')
+	#return HttpResponse('Projeto Inicial')
+	return render(request, 'home.html', {'usuario' : 'Idnilson'})
